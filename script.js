bacf4a0a13cbc4913d2a9313995466298a146c58
leandro-lcistemas <leandro.nascimento@lcsistemas.com.br>
2023-05-05 16:49:06 -0300

    adição de solicitação de altorização

--- script.js
+++ script.js
@@ -3,10 +3,20 @@ const button = document.getElementById('button');
 button.addEventListener('click', () => {
     const urlParams = new URLSearchParams(window.location.search);
     const code = urlParams.get('code');
-    
-    if (!code) {
-        console.log("No code provided");
+
+    if (code) {
+        fetch('http://localhost:4803/api/cora/gerar-token', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ code: code })
+        })
+            .then(response => response.json())
+            .then(data => console.log(data))
+            .catch(error => console.error(error));
+    } else {
+        console.log('Não há código de autorização');
     }
 
-    console.log("Code: " + code);
 });
