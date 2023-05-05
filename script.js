539c3491acb7d30e08db9ba0c301ed0f056434c2
leandro-lcistemas <leandro.nascimento@lcsistemas.com.br>
2023-05-05 16:51:57 -0300

    add auth

--- script.js
+++ script.js
@@ -8,7 +8,8 @@ button.addEventListener('click', () => {
         fetch('http://localhost:4803/api/cora/gerar-token', {
             method: 'POST',
             headers: {
-                'Content-Type': 'application/json'
+                'Content-Type': 'application/json',
+                'Authorization': 'Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJ3aWxsaWFtLnJvZHJpZ3VlcyIsImlhdCI6MTY4MzMxNTM2OSwiZXhwIjoxNjgzMzg3MzY5fQ.88F7UdKnINntkJ8ofWZa86I5MI6myoKvt-1dNhnPtPipFMW0yJPXJ41_gHlZFJ9BUV9WtjGSrfTijLFdTzOXVQ'
             },
             body: JSON.stringify({ code: code })
         })
