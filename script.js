f8bc9eeb998bdc46a4488579635e54cf1c117c29
leandro-lcsistemas <leandro.nascimento@lcsistemas.com.br>
2023-05-15 14:23:29 -0300

    commit

--- script.js
+++ script.js
@@ -10,14 +10,14 @@ button.addEventListener('click', () => {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
-                'Authorization': 'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJsdWl6LmNhcmxvcyIsImlhdCI6MTY4NDE3MDkxMSwiZXhwIjoxNjg0MjQyOTExfQ.e9Ri7czuNOay63yIcbHEQTw4XcXBuWKQ7XCnUu7QIG8bmvsT_abhWU80k3SHX9kBW6jvnBGvDdrX4N7eSbbYWw'
+                'Authorization': 'Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJsdWl6LmNhcmxvcyIsImlhdCI6MTY4NDE3MDkxMSwiZXhwIjoxNjg0MjQyOTExfQ.e9Ri7czuNOay63yIcbHEQTw4XcXBuWKQ7XCnUu7QIG8bmvsT_abhWU80k3SHX9kBW6jvnBGvDdrX4N7eSbbYWw'
             },
             body: JSON.stringify({ code: code })
         })
             .then(response => response.json())
             .then(data => {
                 console.log(data);
-                window.location.href = "http://localhost:3000/app/info-revenda"
+                window.location.href = "http://localhost:3000/Login"
                 window.alert('autorizado com sucesso')
             })
             .catch(error => console.error(error));
