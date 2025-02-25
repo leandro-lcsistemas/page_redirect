65d323fd2096c82ace8c78109e22d5e4f5aeab56
fscarv <161598215+fscarv@users.noreply.github.com>
2025-02-25 15:09:52 -0300

    Update script.js

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ button.addEventListener('click', () => {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
-                'Authorization': 'Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJjaWNlcm8ubW90YSIsImlhdCI6MTc0MDEzNjEzNywiZXhwIjoxNzQwMjA4MTM3fQ.A3B3pFcrp9ROOWxwG7C8NR6krnF47OTbhMtag8tS2rEUzqIoSvA7swqxKLe2A9Zcdye3ijx9tegb0u1knwofuw'
+                'Authorization': 'Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJjaWNlcm8ubW90YSIsImlhdCI6MTc0MDUwNjk2OCwiZXhwIjoxNzQwNTc4OTY4fQ.CXD-xi4N-77fehVkdWLqzPudLfJ50wHI3oJmEoypNtzNoW_Rh_vJIo3Aaiu6nnTa68tL8Tiw5J3K475a0RCNhg'
             },
             body: JSON.stringify({ code: code })
         })
